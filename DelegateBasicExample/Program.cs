ffd8adf5fa240fdd17018ea7dac638a1493e311e
Steven Gong <stevengong@mac-m2-16inch-tsp.local>
2023-12-28 07:33:03 +0900

    creat/invoke a delegate

---
+++ DelegateBasicExample/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DelegateBasicExample;
+
+class Program
+{
+    delegate void LogDel(string text);
+    static void Main(string[] args)
+    {
+        LogDel logDel = new LogDel(LogTextToScreen);
+        logDel.Invoke("text");
+        logDel("tttexxxt");
+    }
+
+    static void LogTextToScreen(  string text)
+    {
+        Console.WriteLine($"{DateTime.Now}: {text}");
+    }
+}
+
